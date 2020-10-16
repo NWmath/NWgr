9b125e0af33ebdfd12e15fc3c57812012309dad6
Andrew Lumsdaine <al75@uw.edu>
2020-10-15 17:35:47 -0700

    first pass at hydizing

---
+++ docs/sphinx/_scripts/generate_hyde.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#very simple for now, look in common locations
+find_hyde() {
+    BUILD_DIR=$1
+    if [ -f $BUILD_DIR/hyde ]
+    then
+        echo $BUILD_DIR/hyde
+        return 0
+    fi
+    if [ -f $BUILD_DIR/Debug/hyde ]
+    then
+        echo $BUILD_DIR/Debug/hyde
+        return 0
+    fi
+    if [ -f $BUILD_DIR/Release/hyde ]
+    then
+        echo $BUILD_DIR/Release/hyde
+        return 0
+    fi
+    return 1
+}   
+
+EXE_DIR=$(dirname "$0")
+
+pushd ${EXE_DIR} > /dev/null
+
+#HYDE_ROOT=/Users/lums658/Contrib/hyde
+
+HYDE_ROOT=/tmp/foo/NWgr/docs/sphinx/_hyde
+
+
+HYDE_PATH=`find_hyde "${HYDE_ROOT}/build"`
+echo HYDE_PATH is ${HYDE_PATH}
+
+# NWGRAPH_ROOT=/Users/lums658/Working/NWmath/NWgr
+NWGRAPH_ROOT=/tmp/foo/NWgr
+
+NWGRAPH_SRC=${NWGRAPH_ROOT}/include
+
+HYDE_DST_ROOT=${NWGRAPH_ROOT}/docs/sphinx/libref
+
+NWGRAPH_INCLUDES="-I${NWGRAPH_SRC} -I${NWGRAPH_SRC}/algorithms -I${NWGRAPH_SRC}/util -I/tmp/foo/NWut/include"
+
+for CUR_LIB in ${NWGRAPH_SRC} ${NWGRAPH_SRC}/algorithms ${NWGRAPH_SRC}/util  ${NWGRAPH_SRC}/detail ; do
+    HYDE_SRC_ROOT=${NWGRAPH_SRC}
+
+    for CUR_FILE in ${CUR_LIB}/*.hpp ; do
+	
+	echo "Processing $CUR_FILE"
+	
+	CUR_COMMAND="${HYDE_PATH} -hyde-verbose -hyde-src-root=${HYDE_SRC_ROOT} -hyde-yaml-dir=${HYDE_DST_ROOT} -hyde-update --auto-toolchain-includes "$CUR_FILE" -- -std=c++17 ${NWGRAPH_INCLUDES}"
+
+	echo $CUR_COMMAND
+	eval $CUR_COMMAND
+	
+    done
+done
+
+popd > /dev/null
---
+++ docs/sphinx/index.rst
@@ -0,0 +1,20 @@
+.. NW Graph documentation master file, created by
+   sphinx-quickstart on Wed Oct 14 18:35:52 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to NW Graph's documentation!
+====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
