95589fe97dd2aceed888ea402e4db5480a8435ac
Andrew Lumsdaine <al75@uw.edu>
2020-12-30 21:15:45 -0800

    clean up test subdir a bit

---
+++ test/ranges_and_concepts_test.cpp
@@ -0,0 +1,47 @@
+//
+// This file is part of Standard Graph Library (SGL)
+// (c) Pacific Northwest National Laboratory 2018
+//
+// Licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
+// https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// Author: Andrew Lumsdaine
+//
+
+#include <concepts>
+
+#include "containers/edge_list.hpp"
+#include "containers/compressed.hpp"
+#include "containers/vovos.hpp"
+#include "containers/aolos.hpp"
+#include "graph_concepts.hpp"
+
+#include "common/abstract_test.hpp"
+
+/*template<typename GraphT>
+requires AdjacencyGraph<GraphT>
+void test_func(GraphT& G) {
+  
+}*/
+
+TEST_CASE("test","[test]") {
+    
+  adj_list<> A(5);
+  A.push_back(0, 1);
+  A.push_back(1, 2);
+  A.push_back(0, 2);
+  A.push_back(1, 3);
+  A.push_back(1, 4);
+  std::ranges::begin(A);
+  for(auto&& out : A) {
+    std::cout << "outer" << std::endl;
+    for(auto&& in : out) {
+      std::cout << "in" << std::endl;
+    }
+  }
+  std::ranges::for_each(A, [](auto&& out){
+    std::ranges::for_each(out, [](auto&& in){
+      std::cout << "edge to " << std::get<0>(in) << std::endl;});
+  });
+  //test_func(A);
+}
