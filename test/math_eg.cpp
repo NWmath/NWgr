acc566df9b132032330789b03d92d0fb5d445d45
Andrew Lumsdaine <al75@uw.edu>
2020-12-30 21:15:29 -0800

    clean up test subdir a bit

---
+++ test/math_eg.cpp
@@ -0,0 +1,72 @@
+//
+// This file is part of BGL17 (aka NWGraph aka GraphPack aka the Graph Standard Library)
+// (c) Pacific Northwest National Laboratory 2020
+//
+// Licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
+// https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// Author: Andrew Lumsdaine
+//
+
+#include "containers/compressed.hpp"
+#include "containers/edge_list.hpp"
+#include "linalg.hpp"
+
+using namespace nw::graph;
+using namespace nw::util;
+
+template<typename Mat, typename EdgeList>
+void run_test(EdgeList&& E) {
+  std::vector<double> x = {3.1, -1.4, 1.59};
+  std::vector<double> y = {1.0, 0.0, -1.0};
+
+  Mat B(E);
+  matvec(B, x, y);
+
+  std::cout << std::endl;
+  for (auto&& j : y) {
+    std::cout << j << std::endl;
+  }
+}
+
+int main() {
+
+  run_test<compressed_sparse<0, directed, double>>(edge_list<directed, double>({{0, 0, 1}, {1, 1, 1}, {2, 2, 1}, {0, 1, -3.7}}));
+
+  edge_list<directed, double> E({{0, 0, 1}, {1, 1, 1}, {2, 2, 1}, {0, 1, -3.7}});
+
+  compressed_sparse<0, directed, double> A(E);
+
+  auto& A_range = A;    //= make_edge_range<0>(A);
+  // auto diag = A_range.begin();
+  //  auto&& [ii, jj, vv] = *diag++;
+
+  std::vector<double> x(4);
+
+  vertex_id_t i = 0;
+  std::for_each(A_range.begin(), A_range.end(), [&](auto&& j) {
+    auto diag       = j.begin();
+    auto&& [jj, vv] = *diag++;
+    std::for_each(j.begin(), j.end(), [&](auto&& elt) {
+      auto&& [k, v] = elt;
+      x[i] -= v * x[k];
+    });
+    ++i;
+  });
+
+#if 0
+  {
+    edge_list<directed, double>            A = {{0, 0, 1}, {1, 1, 1}, {2, 2, 1}};
+    compressed_sparse<0, directed, double> B(A);
+
+    matvec(B, x, y);
+
+    for (auto&& j : y) {
+      std::cout << j << std::endl;
+    }
+  }
+
+#endif
+
+  return 0;
+}
