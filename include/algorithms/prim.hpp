149bf1bb10e624573a119f7654aa14b2c0e79174
Andrew Lumsdaine <al75@uw.edu>
2021-01-29 10:55:42 -0800

    add prim and mv spanning_tree to kruskal

---
+++ include/algorithms/prim.hpp
@@ -0,0 +1,24 @@
+//
+// This file is part of Standard Graph Library (SGL)
+// (c) Pacific Northwest National Laboratory 2018
+//
+// Licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0
+// International License https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// Author: Andrew Lumsdaine
+//
+
+#ifndef NW_GRAPH_PRIM_HPP
+#define NW_GRAPH_PRIM_HPP
+
+#include "adaptors/bfs_range.hpp"
+
+namespace nw {
+namespace graph {
+
+
+
+}    // namespace graph
+}    // namespace nw
+
+#endif  // NW_GRAPH_PRIM_HPP
