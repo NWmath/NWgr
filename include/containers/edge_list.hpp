0f9d3fe72ec35c38dd57a437adbb7aa0789388ca
Andrew Lumsdaine <al75@uw.edu>
2021-02-05 09:49:35 -0800

    add access

--- include/containers/edge_list.hpp
+++ include/containers/edge_list.hpp
@@ -1,6 +1,6 @@
 //
-// This file is part of BGL17 (aka NWGraph aka GraphPack aka the Graph Standard Library)
-// (c) Pacific Northwest National Laboratory 2018-2020
+// This file is part of NWGraph aka GraphPack aka the Graph Standard Library
+// (c) Pacific Northwest National Laboratory and University of Washington 2018-2021
 //
 // Licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
 // https://creativecommons.org/licenses/by-nc-sa/4.0/
