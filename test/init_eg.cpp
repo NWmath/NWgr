dae1605534026cbf21f3b3a763398de82698f06f
Andrew Lumsdaine <al75@uw.edu>
2021-02-11 07:51:48 -0800

    add triangular_eg.cpp

--- test/init_eg.cpp
+++ test/init_eg.cpp
@@ -22,22 +22,9 @@ int main() {
     edge_list<directedness::undirected> a{{0, 1}, {1, 2}};
     edge_list<directedness::directed>   b{{0, 2}, {1, 0}, {0, 1}};
 
-    edge_list<directedness::directed>   c = a.convert_directedness<directedness::directed>();
-    edge_list<directedness::undirected> d = b.convert_directedness<directedness::undirected>();
-    edge_list<directedness::directed>   e = b.convert_directedness<directedness::directed>();
-    edge_list<directedness::undirected> f = a.convert_directedness<directedness::undirected>();
-
     a.stream_edges(std::cout);
     std::cout << std::endl;
     b.stream_edges(std::cout);
-    std::cout << std::endl;
-    c.stream_edges(std::cout);
-    std::cout << std::endl;
-    d.stream_edges(std::cout);
-    std::cout << std::endl;
-    e.stream_edges(std::cout);
-    std::cout << std::endl;
-    f.stream_edges(std::cout);
     std::cout << std::endl;
   }
 
@@ -45,22 +32,10 @@ int main() {
     edge_list<directedness::undirected, double> a{{0, 1, 3.1}, {1, 2, 4.159}};
     edge_list<directedness::directed, double>   b{{0, 2, 42.2}, {1, 0, 24.4}, {0, 1, 81.9}};
 
-    auto c = a.convert_directedness<directedness::directed>();
-    auto d = b.convert_directedness<directedness::undirected>();
-    auto e = b.convert_directedness<directedness::directed>();
-    auto f = a.convert_directedness<directedness::undirected>();
 
     a.stream_edges(std::cout);
     std::cout << std::endl;
     b.stream_edges(std::cout);
-    std::cout << std::endl;
-    c.stream_edges(std::cout);
-    std::cout << std::endl;
-    d.stream_edges(std::cout);
-    std::cout << std::endl;
-    e.stream_edges(std::cout);
-    std::cout << std::endl;
-    f.stream_edges(std::cout);
     std::cout << std::endl;
   }
 
---
+++ test/triangular_eg.cpp
@@ -0,0 +1,56 @@
+//
+// This file is part of BGL17 (aka NWGraph aka GraphPack aka the Graph Standard Library)
+// (c) Pacific Northwest National Laboratory 2018
+//
+// Licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
+// https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// Author: Andrew Lumsdaine
+//
+
+#include <iostream>
+
+#include "containers/edge_list.hpp"
+#include "build.hpp"
+
+using namespace nw::graph;
+using namespace nw::util;
+
+int main() {
+  edge_list<directedness::undirected> a{{0, 0}, {1, 0}, {4, 0}, {1, 2}, {1, 1}};
+  edge_list<directedness::undirected> b{{0, 0}, {0, 1}, {0, 4}, {2, 1}, {1, 1}};
+  edge_list<directedness::undirected> c{{0, 0}, {0, 1}, {0, 4}, {1, 0}, {4, 0}, {2, 1}, {1,2}, {2,3},{3,1}};
+  
+  std::cout << "Successor (upper)" << std::endl;
+  a.stream_edges(std::cout);
+  std::cout << std::endl;
+  swap_to_triangular<0>(a, succession::successor);
+  a.stream_edges(std::cout);
+  std::cout << std::endl;
+  
+  std::cout << "Predecessor (lower)" << std::endl;
+  b.stream_edges(std::cout);
+  std::cout << std::endl;
+  swap_to_triangular<0>(b, succession::predecessor);
+  b.stream_edges(std::cout);
+  std::cout << std::endl;
+  
+  std::cout << "Successor (upper)" << std::endl;
+  c.stream_edges(std::cout);
+  std::cout << std::endl;
+  swap_to_triangular<0>(c, succession::successor);
+  c.stream_edges(std::cout);
+  std::cout << std::endl;
+  
+  std::cout << "Sort" << std::endl;
+  lexical_sort_by<0>(c);
+  c.stream_edges(std::cout);
+  std::cout << std::endl;
+  
+  std::cout << "Uniq" << std::endl;
+  uniq(c);
+  c.stream_edges(std::cout);
+  std::cout << std::endl;
+
+  return 0;
+}
