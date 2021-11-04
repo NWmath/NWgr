8d99e04a31834da9b82d004c454f335c99f26bbe
Tony Liu <xu.liu2@wsu.edu>
2021-11-04 10:58:06 -0700

    split experimental js

--- include/nwgraph/experimental/algorithms/jaccard.hpp
+++ include/nwgraph/experimental/algorithms/jaccard.hpp
@@ -9,8 +9,8 @@
 //     Andrew Lumsdaine	
 //
 
-#ifndef NW_GRAPH_JACCARD_HPP
-#define NW_GRAPH_JACCARD_HPP
+#ifndef NW_GRAPH_EXPERIMENTAL_JACCARD_HPP
+#define NW_GRAPH_EXPERIMENTAL_JACCARD_HPP
 
 #include "nwgraph/adaptors/cyclic_range_adapter.hpp"
 #include "nwgraph/util/parallel_for.hpp"
@@ -29,18 +29,15 @@
 namespace nw {
 namespace graph {
 
-
-
 template <typename GraphT>
-size_t jaccard_similarity_v0(const GraphT& A) {
+size_t jaccard_similarity_v1(const GraphT& A) {
   size_t ctr = 0;
   auto   first     = A.begin();
   auto   last      = A.end();
 
-
   for (auto G = first; first != last; ++first) {
     for (auto v = (*first).begin(); v != (*first).end(); ++v) {
-      auto numer = nw::graph::intersection_size(*first, G[std::get<0>(*v)]);
+      auto numer = nw::graph::intersection_size(v (*first).end(), G[std::get<0>(*v)]);
       auto denom = degree(*first) + degree(G[std::get<0>(*v)]) - numer;
       double rat   = ((double)numer) / ((double)denom);
       std::get<2>(*v) = rat;
@@ -50,9 +47,25 @@ size_t jaccard_similarity_v0(const GraphT& A) {
   return ctr;
 }
 
+#if 0
+template <typename GraphT>
+auto jaccard_similarity_v2(GraphT& graph) {
+
+  size_t ctr = 0;
+  auto deg = degrees(graph);
+  for (auto&& [u, v, w] : make_edge_range<0, 1, 2>(graph)) {
+    auto   numer = intersection_size(graph[u], graph[v]);
+    auto   denom = deg[u] + deg[v] - numer;
+    double rat   = ((double)numer) / ((double)denom);
+    w            = rat;
+    ++ctr;
+  }
+  return ctr;
+}
+#endif
 
 
 }    // namespace graph
 }    // namespace nw
 
-#endif    // NW_GRAPH_JACCARD_HPP
+#endif    // NW_GRAPH_EXPERIMENTAL_JACCARD_HPP
