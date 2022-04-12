56fd6a225d7e92759e6fd22494fc87fe76b37e79
Andrew Lumsdaine <al75@uw.edu>
2022-04-12 12:28:06 -0700

    rough draft implementation of prim

--- include/nwgraph/algorithms/prim.hpp
+++ include/nwgraph/algorithms/prim.hpp
@@ -38,17 +38,53 @@ namespace graph {
 /**
  * @brief Prim's minimum spanning tree algorithm
  * A greedy algorithm for a weighted undirected graph
- * @todo implemenent a basic sequential prim algorithm
- * @tparam DistanceT 
- * @tparam GraphT 
- * @param graph 
- * @param source 
- * @return std::vector<DistanceT> 
+ * @tparam Graph Graph type.  Must meet requirements of adjacency_list_graph.
+ * @tparam Distance Type of edge weight.
+ * @tparam Weight A weight function for a given edge, returns a Distance.
+ * @param graph Input graph.
+ * @param source Starting vertex.
+ * @return std::vector<vertex_id_t<Graph>>, the predecessor list (the MST).
  */
-template <class DistanceT, adjacency_list_graph GraphT>
-std::vector<DistanceT> prim(const GraphT& graph, vertex_id_t<GraphT> source) {
-  std::vector<DistanceT> ret;
-  return ret;
+template <adjacency_list_graph Graph, class Distance, class Weight>
+std::vector<vertex_id_t<Graph>> prim(const Graph& graph, vertex_id_t<Graph> source, Weight&& weight) {
+
+  using vertex_id_type = vertex_id_t<Graph>;
+
+  size_t N { num_vertices(graph) };
+  assert(source < N);
+
+  std::vector<Distance> distance(N, std::numeric_limits<Distance>::max());
+  std::vector<Distance> predecessor(N, std::numeric_limits<Distance>::max());
+  std::vector<uint8_t> finished(N, false);
+  distance[source] = 0;
+
+  using weight_t        = Distance;
+  using weighted_vertex = std::tuple<vertex_id_type, weight_t>;
+
+  std::priority_queue<weighted_vertex, std::vector<weighted_vertex>, std::greater<weighted_vertex>> Q;
+
+  while (!Q.empty()) {
+
+    auto u = std::get<0>(Q.top());
+    Q.pop();
+
+    if (finished[u]) {
+      continue;
+    }
+
+    std::for_each(g[u].begin(), g[u].end(), [&](auto&& e) {
+      auto v = target(graph, e);
+      auto w = weight(e);
+
+      if (!finished[v] && distance[v] > w) {
+	distance[v] = w;
+	Q.push({ v, distance[v] });
+	predecessor[v] = u;
+      }
+    });
+  }
+
+  return predecessor;
 }
 
 }    // namespace graph
